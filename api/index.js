41c50dacd7506d4f8a6233077074bdf10671581b
itsdeepee <0x4deepee@gmail.com>
2024-02-05 20:06:35 +0200

    connect to databasse

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 
 import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config'
 
+mongoose.connect(process.env.REACT_APP_MONGO).then(()=>{
+    console.log("Connected to MongoDB!");
+}).catch((err)=>{
+    console.log(err);
+})
 
 const app=express();
 
